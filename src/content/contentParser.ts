7856842954cb817fef84c0285c82f2ff5d6c87fd
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2024-11-05 21:23:28 +0100

    feat: generate parser from component (#2)
    Let the user generate parser from the components. 
    
    ```ts
    const parsePageContentAuto = contentParser(pageComponent)
    ```
    
    If they would like to customize it, they can just create a parser
    themselves with explicit type annotation:
    
    ```ts
    type PageContent = ContentFromComponent<typeof pageComponent>
    
    const parsePageContent = blockContent<PageContent>({
      _uid: parseUuid,
      component: literal('page'),
      padding: parseNumberContent,
      isPublic: parseBooleanContent,
      title: parseTextContent,
    })
    ```

---
+++ src/content/contentParser.ts
@@ -0,0 +1,33 @@
+import {
+  Component,
+  ContentFromComponent,
+  Field,
+  ContentFromField,
+} from '../component'
+import { literal, object, Parser, parseString } from 'pure-parse'
+import { parseBooleanContent } from './booleanContent'
+import { parseTextContent } from './textContent'
+import { parseNumberContent } from './numberContent'
+
+const contentParserFromField: {
+  [K in Field['type']]: Parser<ContentFromField<Extract<Field, { type: K }>>>
+} = {
+  boolean: parseBooleanContent,
+  text: parseTextContent,
+  number: parseNumberContent,
+}
+
+export const contentParser = <C extends Component>(
+  component: C,
+): Parser<ContentFromComponent<C>> =>
+  // @ts-ignore
+  object<ContentFromComponent<C>>({
+    _uid: parseString,
+    component: literal(component.name),
+    schema: Object.fromEntries(
+      Object.entries(component.schema).map(([key, value]) => [
+        key,
+        contentParserFromField[value.type],
+      ]),
+    ),
+  })
